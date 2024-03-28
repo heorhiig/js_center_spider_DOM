1a5858cc8d6e51bb68993731bdc5843be298d9df
Heorhii Churhuliia <gio4929189@gmail.com>
2024-03-28 17:02:09 +0000

    make build

---
+++ index.a27deb29.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=(e.clientHeight-t.clientHeight)/2,l=(e.clientWidth-t.clientWidth)/2;t.style.top="".concat(l,"px"),t.style.left="".concat(c,"px");
+//# sourceMappingURL=index.a27deb29.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const y = (wall.clientHeight - spider.clientHeight) / 2;
+const x = (wall.clientWidth - spider.clientWidth) / 2;
+spider.style.top = `${x}px`;
+spider.style.left = `${y}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
